0025d58189d5829c7cdb72153cf13d82e9225643
Vinicius Borges <93492742+borgesvinicius@users.noreply.github.com>
2023-09-24 20:48:42 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,78 +1,73 @@
-const charactersList = document.getElementById('charactersList')
-const searchCharactersByName = document.getElementById('searchCharactersByName')
+const charactersList = document.getElementById("charactersList");
+const searchCharactersByName = document.getElementById(
+  "searchCharactersByName"
+);
+const prevPage = document.getElementById("prevPage");
+const nextPage = document.getElementById("nextPage");
+let response;
+let currentPage = 1;
+let isLoading = false;
 
-const prevPage = document.getElementById('prevPage') 
-const nextPage = document.getElementById('nextPage') 
-
-let response
-let currentPage = 1
-
-let isLoading = false
-
-async function loadCharacters(page = 1, name = '') {
+async function loadCharacters(page = 1, name = "") {
   try {
-    isLoading = true
-
+    isLoading = true;
     const params = {
       name,
-      page
-    }
+      page,
+    };
+    response = await api.get("/character", { params });
+    prevPage.disabled = true;
+    nextPage.disabled = true;
+    charactersList.innerHTML = "";
 
-    response = await api.get('/character', { params })
+    console.log(response.data);
 
-    prevPage.disabled = true
-    nextPage.disabled = true
+    response.data.results.forEach((character) => {
+      const characterCard = document.createElement("div");
+      characterCard.classList.add("characters-card");
 
-    charactersList.innerHTML = ''
+      const characterImage = document.createElement("img");
+      characterImage.classList.add("characters-image");
+      characterImage.src = character.image;
 
-    console.log(response.data)
+      const statusClass =
+        character.status === "Alive" ? "status-alive" : "status-dead";
 
-    response.data.results.forEach(character => {
-      const characterCard = document.createElement('div')
-      characterCard.classList.add('characters-card')
+      const characterName = document.createElement("h2");
+      characterName.textContent = character.name;
+      characterName.classList.add(statusClass);
 
-      const characterImage = document.createElement('img')
-      characterImage.classList.add('characters-image')
-      characterImage.src = character.image
+      characterCard.appendChild(characterImage);
+      characterCard.appendChild(characterName);
+      charactersList.appendChild(characterCard);
+    });
 
-      const statusClass = character.status === 'Alive' ? 'status-alive' : 'status-dead'
-
-      const characterName = document.createElement('h2')
-      characterName.textContent = character.name
-      characterName.classList.add(statusClass)
-
-      characterCard.appendChild(characterImage)
-      characterCard.appendChild(characterName)
-
-      charactersList.appendChild(characterCard)
-
-
-      prevPage.disabled = response.data.info.prev ? false : true
-      nextPage.disabled = response.data.info.next ? false : true
-    })
-
+    prevPage.disabled = !response.data.info.prev;
+    nextPage.disabled = !response.data.info.next;
   } catch (error) {
-    console.log("Erro ao buscar personagens.", error)
+    console.log("Erro ao buscar personagens.", error);
   } finally {
-    isLoading = false
+    isLoading = false;
   }
 }
 
-loadCharacters()
+loadCharacters();
 
-searchCharactersByName.addEventListener('input', () => {
-  currentPage = 1
-  loadCharacters(currentPage, searchCharactersByName.value)
-})
+searchCharactersByName.addEventListener("input", () => {
+  currentPage = 1;
+  loadCharacters(currentPage, searchCharactersByName.value);
+});
 
-prevPage.addEventListener('click', () => {
+prevPage.addEventListener("click", () => {
   if (currentPage > 1 && !isLoading) {
-    currentPage--
-    loadCharacters(currentPage)
+    currentPage--;
+    loadCharacters(currentPage);
   }
-})
+});
 
-nextPage.addEventListener('click', () => {
+nextPage.addEventListener("click", () => {
   if (currentPage < response.data.info.pages && !isLoading) {
-    currentPage++
-    loadCharacters(currentPage)
+    currentPage++;
+    loadCharacters(currentPage);
+  }
+});
