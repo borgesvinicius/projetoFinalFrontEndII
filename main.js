6d4771d295e36d58ffa6c6ac313315ea6336542a
Vinicius Borges <93492742+borgesvinicius@users.noreply.github.com>
2023-09-24 20:06:23 -0300

    chamando os elementos

---
+++ main.js
@@ -0,0 +1,7 @@
+const charactersList = document.getElementById("charactersList");
+const searchCharactersByName = document.getElementById(
+  "searchCharactersByName"
+);
+
+const prevPage = document.getElementById("prevPage");
+const nextPage = document.getElementById("nextPage");
